c9ed11e8d173255a5bfd5492b68919baff5ed884
Fabian Thorand <f.thorand@gmail.com>
2015-05-26 18:02:36 +0200

    Added GLFW startup code.

---
+++ src/Graphics/Ninja/UI.hs
@@ -0,0 +1,58 @@
+-- | A module for quickly setting up a GLFW window.
+module Graphics.Ninja.UI
+       ( withGLFW
+       , WindowMode (..)
+       , openGL33Core
+       ) where
+
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.State
+import qualified Graphics.UI.GLFW     as GLFW
+
+-- | Window hints stating the requirement of OpenGL 3.3 Core profile.
+openGL33Core :: [GLFW.WindowHint]
+openGL33Core =
+  [ GLFW.WindowHint'OpenGLProfile GLFW.OpenGLProfile'Core
+  , GLFW.WindowHint'ContextVersionMajor 3
+  , GLFW.WindowHint'ContextVersionMinor 3
+  ]
+
+-- | Window mode for high-level GLFW wrapper
+data WindowMode
+  = Windowed Int Int
+  | BorderlessFullscreen
+
+-- | High-level GLFW initialization function.
+-- The custom window hints take precedence over the ones derived from the WindowMode.
+withGLFW :: WindowMode -> String -> [GLFW.WindowHint] -> (GLFW.Window -> IO ()) -> IO ()
+withGLFW mode title hints runApp = do
+    liftIO $ GLFW.setErrorCallback (Just onError)
+    bracket GLFW.init (`when` GLFW.terminate) $ const $ do
+        (w,h,mon) <- case mode of
+            Windowed w h -> return (w,h,Nothing)
+            BorderlessFullscreen -> do
+              -- For borderless fullscreen, the resolution and color depth
+              -- has to be exactly the one currently used.
+              mon <- GLFW.getPrimaryMonitor <?> "no primary monitor"
+              vm <- GLFW.getVideoMode mon   <?> "error getting video mode"
+              GLFW.windowHint (GLFW.WindowHint'RedBits $ GLFW.videoModeRedBits vm)
+              GLFW.windowHint (GLFW.WindowHint'GreenBits $ GLFW.videoModeGreenBits vm)
+              GLFW.windowHint (GLFW.WindowHint'BlueBits $ GLFW.videoModeBlueBits vm)
+              GLFW.windowHint (GLFW.WindowHint'RefreshRate $ GLFW.videoModeRefreshRate vm)
+              return (GLFW.videoModeWidth vm, GLFW.videoModeHeight vm, Just mon)
+        mapM_ GLFW.windowHint hints
+        win <- GLFW.createWindow w h title mon Nothing <?> "window creation failed"
+        GLFW.makeContextCurrent (Just win)
+        runApp win `finally` GLFW.destroyWindow win
+  where
+    onError err msg = do
+      putStrLn "********** INIT ERROR **********"
+      print err
+      putStrLn msg
+      putStrLn "********************************"
+
+-- | Makes an @IO (Maybe a)@ action actually fail with an error when @Nothing@ is returned.
+(<?>) :: IO (Maybe a) -> String -> IO a
+act <?> err = act >>= maybe (ioError $ userError err) return
+infixr 0 <?>
